db5d2676ca830aadb68d307f26ef80424f764681
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-10-15 13:19:48 -0300

    Upload dish photo route added

---
+++ API/src/controllers/DishesPhotoController.js
@@ -0,0 +1,34 @@
+const knex = require('../database/knex')
+
+const AppError = require('../utils/AppError')
+const DiskStorage = require('../providers/diskStorage')
+
+class IngredientsPhotoController {
+    async update(req, res) {
+
+        const diskStorage = new DiskStorage()
+
+        const { dish_id } = req.params
+        const photoFilename = req.file.filename 
+        
+        const dish = await knex('dishes').where({ id: dish_id })
+        
+        if(!dish){
+            throw new AppError('Ingrediente não existe')
+        }
+        
+        if(dish[0].photo){
+            await diskStorage.deleteFile(dish.photo)
+        }
+        
+        await diskStorage.saveFile(photoFilename)
+
+        dish[0].photo = photoFilename
+
+        await knex('dishes').update(dish[0]).where({ id: dish_id })
+
+        return res.json(dish)
+    }
+}
+
+module.exports = IngredientsPhotoController
--- API/src/routes/dishes.routes.js
+++ API/src/routes/dishes.routes.js
@@ -2,11 +2,20 @@ const { Router } = require('express')
 
 const dishesRoutes = Router()
 
+const uploadConfig = require('../config/upload')
+const multer = require('multer')
+
+const upload = multer(uploadConfig.MULTER)
+
 const DishesController = require('../controllers/DishesController')
+const DishesPhotoController = require('../controllers/DishesPhotoController')
+
 const dishesController = new DishesController()
+const dishesPhotoController = new DishesPhotoController()
 
 dishesRoutes.post('/', dishesController.create)
 dishesRoutes.get('/:id', dishesController.show)
 dishesRoutes.delete('/:id', dishesController.delete)
+dishesRoutes.patch('/photo/:dish_id', upload.single('photo'), dishesPhotoController.update)
 
 module.exports = dishesRoutes
